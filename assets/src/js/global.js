4e312ffd4fa3474f56b607531742195992a6ad95
Pamela Ribeiro <contato@pamelanascimento.com>
2022-11-10 16:58:09 -0300

    Update global.js

--- assets/src/js/global.js
+++ assets/src/js/global.js
@@ -7,35 +7,7 @@
 
 /**
  * Search for Donation Button and add parameter link Op1.
- * This function is mainting all the parameters and adding 'utm_source' to it.
  */
-
-function search_donation_links() {
-	'use strict';
-
-	var page_id = window.post_id;
-	var donation_button = document.querySelectorAll('[href^="https://donate.wikimedia.org"]');
-
-	function add_parameter( item ) {
-		var current_url = item.getAttribute( 'href' );
-
-		if ( current_url.includes('utm_source') ) {
-			item.href = current_url.replace( /utm_source=0*[1-9][0-9]*/, 'utm_source=' + page_id );
-		} else {
-			item.href = current_url + '&utm_source=' + page_id;
-		}
-	}
-
-	donation_button.forEach(add_parameter);
-}
-
-// 
-
-/**
- * Search for Donation Button and add parameter link Op1.
- * This function is removing all the parameters and adding 'utm_source' to it.
- */
-
 document.querySelectorAll('[href^="https://donate.wikimedia.org"]').forEach( function( item ) {
 	'use strict';
 	var page_id = window.post_id;
