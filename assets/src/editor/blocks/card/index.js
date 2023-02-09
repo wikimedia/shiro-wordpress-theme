87a84a3448a117a35b148fde3135676923cca99f
Pamela Ribeiro <contato@pamelanascimento.com>
2023-02-09 17:26:31 -0300

    Added color styles to Cards block

--- assets/src/editor/blocks/card/index.js
+++ assets/src/editor/blocks/card/index.js
@@ -12,13 +12,15 @@ import { __ } from '@wordpress/i18n';
 import BlockIcon from '../../../svg/blocks/card.svg';
 import CallToActionPicker from '../../components/cta';
 import ImagePicker from '../../components/image-picker';
+import sharedStyles from '../../helpers/block-styles';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-export const name = 'shiro/card';
+export const name = 'shiro/card',
+	styles = sharedStyles;
 
 export const settings = {
 	apiVersion: 2,
