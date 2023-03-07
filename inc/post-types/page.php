cd7279ed2067b02110329f98a51ea646d9234ec4
goldenapples <goldenapplesdesign@gmail.com>
2023-03-07 17:08:44 -0500

    FIx docblocks for linter

--- inc/post-types/page.php
+++ inc/post-types/page.php
@@ -5,7 +5,6 @@
  * @package shiro
  */
 
-
 /**
  * Registers the "hide page title" meta field
  */
@@ -21,6 +20,9 @@ add_action( 'init', 'wmf_page_init' );
 
 /**
  * Filter the body class to include "hide-page-title" if the hide title meta is set.
+ *
+ * @param string[] $body_classes Body classes assigned to the request.
+ * @return string[] Updated body classes.
  */
 function wmf_page_filter_body_class( $body_classes ) {
 	if ( get_post_type() === 'page' && get_post_meta( get_the_ID(), '_wmf_hide_title', true ) ) {
