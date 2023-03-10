18d14032fdb14c90d6d385d295e8a39e46457da1
goldenapples <goldenapplesdesign@gmail.com>
2023-03-10 14:16:52 -0500

    Add options for slide behavior
    Adds controls on the carousel block for arrow, pagination dots,
    autoplay, and slides per page.

--- assets/src/editor/blocks/carousel/index.js
+++ assets/src/editor/blocks/carousel/index.js
@@ -12,6 +12,7 @@ import {
 	RangeControl,
 	SelectControl,
 	TextControl,
+	ToggleControl,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
@@ -62,7 +63,15 @@ export const settings = {
 	edit: function Edit( props ) {
 		const { attributes, clientId, setAttributes } = props;
 
-		const { title, currentBlock, perPage } = attributes;
+		const {
+			title,
+			currentBlock,
+			perPage,
+			arrows,
+			pagination,
+			autoplay,
+			interval,
+		} = attributes;
 
 		const blockProps = useBlockProps( {
 			className: 'shiro-carousel',
@@ -105,7 +114,41 @@ export const settings = {
 							value={ perPage }
 							onChange={ ( perPage ) => setAttributes( { perPage } ) }
 							min={ 1 }
-							max={ 3 }
+							max={ 4 }
+						/>
+						<ToggleControl
+							label={ __( 'Show navigation arrows?', 'shiro-admin' ) }
+							checked={ arrows }
+							onChange={ ( arrows ) => setAttributes( { arrows } ) }
+						/>
+						<ToggleControl
+							label={ __( 'Show pagination dots?', 'shiro-admin' ) }
+							checked={ pagination }
+							onChange={ ( pagination ) => setAttributes( { pagination } ) }
+						/>
+						<ToggleControl
+							label={ __( 'Enable autoplay?', 'shiro-admin' ) }
+							checked={ autoplay }
+							onChange={ ( autoplay ) => setAttributes( { autoplay } ) }
+						/>
+						<RangeControl
+							disabled={ ! autoplay }
+							label={ __( 'Interval between autoplaying slides', 'shiro-admin' ) }
+							value={ interval }
+							onChange={ ( interval ) => setAttributes( { interval } ) }
+							min={ 0 }
+							max={ 10000 }
+							marks={ [ // eslint-ignore
+								{ 'label': '1s', 'value': 1000 },
+								{ 'label': '2s', 'value': 2000 },
+								{ 'label': '3s', 'value': 3000 },
+								{ 'label': '4s', 'value': 4000 },
+								{ 'label': '5s', 'value': 5000 },
+								{ 'label': '6s', 'value': 6000 },
+								{ 'label': '7s', 'value': 7000 },
+								{ 'label': '8s', 'value': 8000 },
+								{ 'label': '9s', 'value': 9000 },
+							] }
 						/>
 					</PanelBody>
 				</InspectorControls>
@@ -131,7 +174,14 @@ export const settings = {
 	save: function Save( props ) {
 		const { attributes } = props;
 
-		const { title, perPage } = attributes;
+		const {
+			title,
+			perPage,
+			arrows,
+			pagination,
+			autoplay,
+			interval,
+		} = attributes;
 
 		const blockProps = useBlockProps.save( {
 			className: classNames( [
@@ -139,6 +189,10 @@ export const settings = {
 			] ),
 			'data-label': title,
 			'data-per-page': perPage,
+			'data-arrows': arrows,
+			'data-pagination': pagination,
+			'data-autoplay': autoplay,
+			'data-interval': autoplay ? interval : null,
 		} );
 
 		const innerBlocksProps = useInnerBlocksProps.save( {
