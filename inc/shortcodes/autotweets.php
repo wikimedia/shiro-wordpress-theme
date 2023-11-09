f78715c510875f135c97b5ce68f2e3ac47cb2d56
varnent <gvarnum@wikimedia.org>
2023-11-08 18:36:58 -0800

    fixes to phpcs - batch 2

--- inc/shortcodes/autotweets.php
+++ inc/shortcodes/autotweets.php
@@ -34,7 +34,6 @@ function wmf_autotweets_callback( $atts = array(), $content = '' ) {
 			'alt'    => array(),
 			'style'  => array(),
 			'class'  => array(),
-			'style'  => array(),
 		),
 		'em'     => array(),
 		'strong' => array(),
