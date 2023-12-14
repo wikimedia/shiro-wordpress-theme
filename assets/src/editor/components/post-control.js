c37c4f7d4ee3d36f10a37780fa2df629a2658130
K. Adam White <kadamwhite@users.noreply.github.com>
2023-12-14 14:02:21 -0500

    Wikimedia-927: Provide wrapper for PostControl to guard against missing dependency
    If the gutenberg tools plugin is inactive, a script error occurs which
    breaks all custom blocks.

---
+++ assets/src/editor/components/post-control.js
@@ -0,0 +1,15 @@
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Fallback component to render if the PostControl is not found.
+ *
+ * This prevents other bundled blocks from failing to initialize if the HM
+ * Gutenberg Tools plugin is not active.
+ */
+const FallbackComponent = () => (
+	<p>{ __( 'HM Gutenberg Tools not found' ) }</p>
+);
+
+const PostControl = hm?.controls?.PostControl || FallbackComponent;
+
+export default PostControl;
