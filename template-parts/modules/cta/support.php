3600d3bbf32fc828372c7575b55b21748089d267
K. Adam White <kadamwhite@users.noreply.github.com>
2024-10-16 15:21:16 +0900

    1017: Ensure templated block areas always layout as expected

--- template-parts/modules/cta/support.php
+++ template-parts/modules/cta/support.php
@@ -9,7 +9,7 @@ $reusable_block = wmf_get_reusable_block_module( 'support' );
 
 if ( $reusable_block ) {
 	if ( is_a( $reusable_block, \WP_Post::class ) ) { ?>
-		<div class="block-area">
+		<div class="block-area is-layout-constrained">
 				<?php
 				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped, WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
 				echo apply_filters( 'the_content', $reusable_block->post_content );
--- template-parts/modules/related/posts.php
+++ template-parts/modules/related/posts.php
@@ -23,7 +23,7 @@ $rand_translation_title = wmf_get_random_translation( 'wmf_related_posts_title' 
 
 ?>
 
-<div class="block-area">
+<div class="block-area is-layout-constrained">
 		<?php
 		/*
 		This uses the structure & styles of the double heading, but can't
