01e757a19f159c6283aa72dce7b4a7c2f19b95b7
Allyson Souza <2000638+allysonsouza@users.noreply.github.com>
2025-01-09 15:44:04 -0300

    Add phpcsignore for nonce verification

--- single-profile.php
+++ single-profile.php
@@ -27,6 +27,8 @@ while ( have_posts() ) :
 	if ( ! empty( $roles ) && ! is_wp_error( $roles ) ) {
 		$team_name  = $roles;
 		$ancestors  = get_ancestors( $roles[0]->term_id, 'role' );
+
+		// phpcs:ignore Processing form data without nonce verification.
 		$referer_id = isset( $_GET['referer'] ) ? absint( $_GET['referer'] ) : null;
 		$referer_link = get_term_link( $referer_id );
 
