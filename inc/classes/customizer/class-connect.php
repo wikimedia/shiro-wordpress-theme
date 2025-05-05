f95ab2231394bd615e618f0620072af6b7a4000a
Joeleen Kennedy <joeleen@humanmade.com>
2025-05-05 17:42:36 -0400

    Replace the shiro mailchimp block with plugin

--- inc/classes/customizer/class-connect.php
+++ inc/classes/customizer/class-connect.php
@@ -13,26 +13,6 @@ namespace WMF\Customizer;
  */
 class Connect extends Base {
 	/**
-	 * Get the default text for a field defined for the Contact customizer.
-	 *
-	 * These are defined here because the Customizer values are not saved, and
-	 * are needed in get_theme_mod() calls in templates. This allows for a
-	 * centralized location for this values and reduces duplication.
-	 *
-	 * @param string $setting Setting key.
-	 *
-	 * @return string
-	 */
-	public static function defaults( string $setting = '' ): string {
-		$defaults = [
-			'wmf_subscribe_action'       => 'https://wikimediafoundation.us11.list-manage.com/subscribe/post?u=7e010456c3e448b30d8703345&amp;id=246cd15c56',
-			'wmf_subscribe_group_number' => '4037',
-		];
-
-		return $defaults[ $setting ] ?? '';
-	}
-
-	/**
 	 * Add Customizer fields for header section.
 	 */
 	public function setup_fields() {
@@ -55,7 +35,7 @@ class Connect extends Base {
 
 		$selectable_blocks = [];
 		foreach ( $reusable_blocks as $block ) {
-			if ( has_block( 'shiro/contact', $block->ID ) || has_block( 'shiro/mailchimp-subscribe', $block->ID ) ) {
+			if ( has_block( 'shiro/contact', $block->ID ) ) {
 				$selectable_blocks[ $block->ID ] = $block->post_title;
 			}
 		}
@@ -71,33 +51,9 @@ class Connect extends Base {
 				'description' => count( $selectable_blocks ) > 0
 					? __( 'Select a reusable block to be shown in the "Connect" area.', 'shiro-admin' )
 					/* translators: %s: Link to the admin page for creating a new reusable block. */
-					: sprintf( __( '<strong>There are no viable reusable blocks!</strong> This reusable block must include at least one of the the Connect or the Mailchimp Subscribe blocks. Please <a href="%s">create one</a>.',
+					: sprintf( __( '<strong>There are no viable reusable blocks!</strong> This reusable block must include the Connect block. Please <a href="%s">create one</a>.',
 					'shiro-admin' ), admin_url( 'edit.php?post_type=wp_block' ) ),
 			]
-		);
-
-		$control_id = 'wmf_subscribe_action';
-		$this->customize->add_setting( $control_id, [
-			'default' => $this::defaults( 'wmf_subscribe_action' ),
-		] );
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Subscribe form action URL', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
-		);
-
-		$control_id = 'wmf_subscribe_group_number';
-		$this->customize->add_setting( $control_id, [
-			'default' => $this::defaults( 'wmf_subscribe_group_number' ),
-		] );
-		$this->customize->add_control(
-			$control_id, array(
-				'label'   => __( 'Subscribe form group number', 'shiro-admin' ),
-				'section' => $section_id,
-				'type'    => 'text',
-			)
 		);
 	}
 
