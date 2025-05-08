e1e7208e0b25e3ea604b2ce4c4606e1129a85e09
Joeleen Kennedy <joeleen@humanmade.com>
2025-05-08 15:18:38 -0400

    Apply spacing sizes to new patterns

--- patterns/header-light.php
+++ patterns/header-light.php
@@ -13,8 +13,8 @@
 
 ?>
 
-<!-- wp:group {"tagName":"header","metadata":{"name":"Header"},"align":"full","className":"site-header","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|main"}}},"border":{"bottom":{"color":"var:preset|color|border-light","width":"1px"},"top":[],"right":[],"left":[]},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
-<header class="wp-block-group alignfull site-header has-base-background-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--border-light);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<!-- wp:group {"tagName":"header","metadata":{"name":"Header"},"align":"full","className":"site-header","style":{"spacing":{"padding":{"top":"var:preset|spacing|24","bottom":"var:preset|spacing|24"}},"elements":{"link":{"color":{"text":"var:preset|color|main"}}},"border":{"bottom":{"color":"var:preset|color|border-light","width":"1px"},"top":[],"right":[],"left":[]},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
+<header class="wp-block-group alignfull site-header has-base-background-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--border-light);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--24);padding-bottom:var(--wp--preset--spacing--24)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 <?php
 /**
  * Available translations are calculated here because we need to know it to
@@ -35,7 +35,7 @@ $translations = array_filter( wmf_get_translations(), function ( $translation ) 
 		<?php endif; ?>>
 	<!-- wp:site-logo {"width":144,"shouldSyncIcon":false} /-->
 
-	<!-- wp:navigation {"ref":77177,"openSubmenusOnClick":true,"icon":"menu","style":{"spacing":{"blockGap":"var:preset|spacing|small"},"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} /-->
+	<!-- wp:navigation {"ref":77177,"openSubmenusOnClick":true,"icon":"menu","style":{"spacing":{"blockGap":"var:preset|spacing|16"},"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} /-->
 	<?php get_template_part( 'template-parts/site-header/language-switcher', null, [ 'translations' => $translations ] ); ?>
 	<!-- wp:buttons -->
 	<div class="wp-block-buttons"><!-- wp:button -->
