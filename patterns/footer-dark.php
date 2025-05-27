7a8111d5154ea53636cce5c9d1b24ce02effc41b
Joeleen Kennedy <joeleen@humanmade.com>
2025-05-27 18:34:14 -0400

    Start on footer content/styles

--- patterns/footer-dark.php
+++ patterns/footer-dark.php
@@ -13,22 +13,22 @@
 
 ?>
 
-<!-- wp:group {"tagName":"footer","metadata":{"name":"Footer"},"align":"full","className":"site-footer","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|32"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"main","textColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
-<footer class="wp-block-group alignfull site-footer has-base-color has-main-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);"><!-- wp:columns {"metadata":{"name":"Org Info Columns"},"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<!-- wp:group {"tagName":"footer","metadata":{"name":"Footer"},"align":"full","className":"site-footer","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|32"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"black-75","textColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
+<footer class="wp-block-group alignfull site-footer has-base-color has-black-75-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:columns {"metadata":{"name":"Org Info Columns"},"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":77235,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/src/images/logo-stacked-reverse.svg" alt="" class="wp-image-77235"/></figure>
+<div class="wp-block-column"><!-- wp:image {"id":77235,"width":"auto","height":"80px","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://wikimedia.vipdev.lndo.site/wp-content/themes/shiro/assets/src/images/logo-stacked-reverse.svg" alt="" class="wp-image-77235" style="width:auto;height:80px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:paragraph {"textColor":"main-accent","fontSize":"small"} -->
-<p class="has-small-font-size">Easily create beautiful, fully-customizable websites with the new WordPress Site Editor and the Shiro block theme.</p>
+<div class="wp-block-column"><!-- wp:paragraph {"className":"has-small-font-size","fontSize":"small"} -->
+<p class="has-small-font-size">Follow the Wikimedia Foundation on social media to get news and updates on our projects, global community, and advocacy efforts.</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:social-links {"iconColor":"main","iconColorValue":"#150E29","iconBackgroundColor":"base","iconBackgroundColorValue":"#fff","className":"is-style-default","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|16","left":"var:preset|spacing|16"}}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<!-- wp:social-links {"iconColor":"base","iconColorValue":"#fff","iconBackgroundColor":"black-75","iconBackgroundColorValue":"#404040","className":"is-style-default","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|16","left":"var:preset|spacing|16"}}},"layout":{"type":"flex","justifyContent":"left"}} -->
 <ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"https://www.instagram.com/wikimediafoundation/","service":"instagram"} /-->
 
 <!-- wp:social-link {"url":"https://bsky.app/profile/wikimediafoundation.org","service":"bluesky"} /-->
@@ -42,29 +42,63 @@
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"></div>
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":77309,"width":"auto","height":"80px","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://wikimedia.vipdev.lndo.site/wp-content/uploads/2025/05/Wikipedia-logo-stacked-reverse.svg" alt="" class="wp-image-77309" style="width:auto;height:80px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"className":"has-small-font-size","fontSize":"small"} -->
+<p class="has-small-font-size">Connect with Wikipedia on social media to learn new facts and know your world better.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"base","iconColorValue":"#fff","iconBackgroundColor":"black-75","iconBackgroundColorValue":"#404040","className":"is-style-default","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|16","left":"var:preset|spacing|16"}}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"tiktok"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"bluesky"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"youtube"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
+
+<!-- wp:separator {"className":"is-style-wide","backgroundColor":"base"} -->
+<hr class="wp-block-separator has-text-color has-base-color has-alpha-channel-opacity has-base-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
 
 <!-- wp:columns {"metadata":{"name":"Nav Columns"}} -->
 <div class="wp-block-columns"><!-- wp:column {"metadata":{"name":"Nav Column"}} -->
-<div class="wp-block-column"><!-- wp:navigation {"ref":77236,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
+<div class="wp-block-column"><!-- wp:navigation {"ref":77310,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"metadata":{"name":"Nav Column"}} -->
-<div class="wp-block-column"><!-- wp:navigation {"ref":77237,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
+<div class="wp-block-column"><!-- wp:navigation {"ref":77314,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"metadata":{"name":"Nav Column"}} -->
-<div class="wp-block-column"><!-- wp:navigation {"ref":77238,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
+<div class="wp-block-column"><!-- wp:navigation {"ref":77316,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"metadata":{"name":"Nav Column"}} -->
-<div class="wp-block-column"><!-- wp:navigation {"ref":77173,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
+<div class="wp-block-column"><!-- wp:navigation {"ref":77249,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
-<!-- wp:paragraph -->
-<p>Except where otherwise noted, the content of this site is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">a Creative Commons Attribution-ShareAlike 4.0 International license</a>. Powered by WordPress.com VIP.</p>
+<!-- wp:separator {"className":"is-style-wide","backgroundColor":"base"} -->
+<hr class="wp-block-separator has-text-color has-base-color has-alpha-channel-opacity has-base-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Except where otherwise noted, the content of this site is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">a Creative Commons Attribution-ShareAlike 4.0 International license</a>. Powered by WordPress.com VIP.</p>
 <!-- /wp:paragraph --></footer>
 <!-- /wp:group -->
