c4d745128c2d846f4f27b42efaec93984a66a31d
Joeleen Kennedy <joeleen@humanmade.com>
2025-05-30 16:53:26 -0400

    Exclude header/footer from background link styles

--- patterns/mega-languages.php
+++ patterns/mega-languages.php
@@ -14,5 +14,5 @@
 ?>
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-base-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:shiro/language-switcher-list /--></div>
+<div class="wp-block-group has-base-background-color has-background has-link-color" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:shiro/language-switcher-list /--></div>
 <!-- /wp:group -->
--- patterns/mega-search.php
+++ patterns/mega-search.php
@@ -14,5 +14,5 @@
 ?>
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|64","bottom":"var:preset|spacing|64"}}},"backgroundColor":"black-05","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-black-05-background-color has-background" style="padding-top:var(--wp--preset--spacing--64);padding-bottom:var(--wp--preset--spacing--64)"><!-- wp:search {"label":"Search","placeholder":"Search site","buttonText":"Search"} /--></div>
+<div class="wp-block-group has-black-05-background-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--64);padding-bottom:var(--wp--preset--spacing--64)"><!-- wp:search {"label":"Search","placeholder":"Search site","buttonText":"Search"} /--></div>
 <!-- /wp:group -->
