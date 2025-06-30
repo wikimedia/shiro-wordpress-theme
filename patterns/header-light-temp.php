980114407377827d9c5dee983c4e0f288df8ef7b
Joeleen Kennedy <joeleen@humanmade.com>
2025-06-30 18:38:04 -0400

    Start updating site header to use current nav

--- patterns/header-light-temp.php
+++ patterns/header-light-temp.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Header Light
- * Slug: shiro/header-light
+ * Title: Header Light Temp
+ * Slug: shiro/header-light-temp
  * Description: Header with nav, search, language, and donate button
  * Categories: header
  * Keywords: header, nav, links, button
@@ -17,7 +17,9 @@
 <header class="wp-block-group alignfull site-header has-base-background-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--border-light);border-bottom-width:1px;padding-top:0;padding-bottom:0"><!-- wp:group {"align":"wide","className":"site-header\u002d\u002dinner","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide site-header--inner"><!-- wp:site-logo {"width":172,"shouldSyncIcon":false} /-->
 
-<!-- wp:navigation {"ref":77251,"openSubmenusOnClick":true,"overlayMenu":"always","icon":"menu","className":"primary-nav","style":{"spacing":{"blockGap":"0"},"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+<!-- wp:navigation {"ref":77571,"overlayMenu":"always","align":"wide","className":"primary-nav mobile-nav","layout":{"type":"flex","justifyContent":"center"}} /-->
+
+<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search site","buttonText":"Search","buttonPosition":"no-button","buttonUseIcon":true,"className":"desktop-nav"} /-->
 
 <!-- wp:navigation {"ref":77284,"overlayMenu":"never","className":"language-switcher-nav","style":{"spacing":{"blockGap":"0"}}} /-->
 
@@ -27,4 +29,10 @@
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></header>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Header"},"align":"full","className":"site-header desktop-nav","style":{"elements":{"link":{"color":{"text":"var:preset|color|main"}}},"border":{"bottom":{"color":"var:preset|color|border-light","width":"1px"},"top":[],"right":[],"left":[]},"position":{"type":""},"spacing":{"padding":{"top":"var:preset|spacing|16","bottom":"var:preset|spacing|16"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"base","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull site-header desktop-nav has-base-background-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--border-light);border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--16);padding-bottom:var(--wp--preset--spacing--16)"><!-- wp:group {"align":"wide","className":"site-header\u002d\u002dinner","style":{"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide site-header--inner"><!-- wp:navigation {"ref":77551,"overlayMenu":"never","align":"wide","className":"primary-nav","layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
