16060826e5011eaff5694b6d2cac99667d0b35d1
Joeleen Kennedy <joeleen@humanmade.com>
2025-07-03 17:55:29 -0400

    Add initial block pattern

---
+++ patterns/hero-rotate-text.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Hero - rotating text
+ * Slug: shiro/hero-rotate-text
+ * Description: Hero block pattern with highlighted rotating text.
+ * Categories: wikimedia
+ * Keywords: 
+ * Viewport Width: 1500
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:group {"align":"full","className":"hero-rotate-text","backgroundColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull hero-rotate-text has-base-background-color has-background"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"jumbo"} -->
+<h2 class="wp-block-heading has-jumbo-font-size">We <span class="text-highlight-carousel">support</span> <br>human knowledge</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"x-large","fontFamily":"display"} -->
+<p class="has-display-font-family has-x-large-font-size">We are the nonprofit that hosts Wikipedia. We support the people, technology, and policies that enable reliable information to be shared with the world.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":77591,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"4px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="https://wikimedia.vipdev.lndo.site/wp-content/uploads/2025/07/cebf6909176a05bdc94cc979df58803ee690491f.jpg?w=1024" alt="" class="wp-image-77591" style="border-radius:4px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
