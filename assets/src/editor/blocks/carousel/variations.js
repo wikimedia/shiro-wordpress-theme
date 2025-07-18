6246c098311a7275de83ce4b15ad0389ca3a5c5c
Joeleen Kennedy <joeleen@humanmade.com>
2025-07-18 16:48:51 -0400

    Add news carousel block variation

---
+++ assets/src/editor/blocks/carousel/variations.js
@@ -0,0 +1,22 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+const variations = [
+	{
+		name: 'shiro/carousel-posts',
+		title: __( 'News carousel' ),
+		description: __(
+			'Carousel block containing a query loop of news posts.'
+		),
+		attributes: {
+			layout: 'carousel-posts',
+			perPage: 3,
+		},
+		isActive: blockAttributes =>
+			blockAttributes.layout === 'carousel-posts',
+	},
+];
+
+export default variations;
