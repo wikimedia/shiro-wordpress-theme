a5f95b2bbeccdb868d54668ef2b3c3e55ec4ca3c
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2025-08-14 09:32:12 -0400

    Add JS and CSS for the tabs

--- assets/src/editor/blocks/tabbed/index.js
+++ assets/src/editor/blocks/tabbed/index.js
@@ -7,6 +7,8 @@ import metadata from './block.json';
 import edit from './edit';
 import save from './save';
 
+import './style.scss';
+
 registerBlockType( metadata.name, {
 	...metadata,
 	edit,
---
+++ assets/src/editor/blocks/tabbed/view.js
@@ -0,0 +1,138 @@
+/**
+ * Functionality for the Tab block.
+ *
+ * Adapted from the code provided on https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/tab_role
+ */
+
+/**
+ * Change to tabs.
+ *
+ * @param {HTMLElement} tab Tab element.
+ */
+function activateTab( tab ) {
+	const blockElement = tab.closest( '.shiro-tabs' );
+	const panel = document.getElementById(
+		tab.getAttribute( 'aria-controls' )
+	);
+	const tabPanels = blockElement.querySelectorAll( '[role="tabpanel"]' );
+
+	// Remove all current selected tabs
+	blockElement
+		.querySelectorAll( '[role="tab"][aria-selected="true"]' )
+		.forEach( ( t ) => {
+			t.setAttribute( 'aria-selected', false );
+			t.setAttribute( 'tabindex', -1 );
+			t.classList.remove( 'shiro-tabs__nav-button--active' );
+		} );
+
+	blockElement
+		.querySelectorAll( '.shiro-tabs__nav-item' )
+		.forEach( ( t ) => {
+			t.classList.remove( 'shiro-tabs__nav-item--active' );
+		} );
+
+	// Set this tab as selected
+	tab.setAttribute( 'aria-selected', true );
+	tab.setAttribute( 'tabindex', 0 );
+	tab.classList.add( 'shiro-tabs__nav-button--active' );
+	tab.closest( '.shiro-tabs__nav-item' ).classList.add(
+		'shiro-tabs__nav-item--active'
+	);
+
+	// Hide all tab panels
+	tabPanels.forEach( ( p ) => {
+		if ( p.getAttribute( 'id' ) === panel.getAttribute( 'id' ) ) {
+			p.removeAttribute( 'hidden' );
+		} else {
+			p.setAttribute( 'hidden', true );
+		}
+	} );
+}
+
+/**
+ * Handle click event on tab.
+ *
+ * @param {object} e Event
+ */
+function handleTabClick( e ) {
+	activateTab( e.target );
+}
+
+/**
+ * Handle key navigation on tab element.
+ *
+ * @param {object} e Event
+ */
+function handleKeydown( e ) {
+	if ( ! ( e.keyCode === 39 || e.keyCode === 37 ) ) {
+		return;
+	}
+
+	const blockElement = e.target.closest( '.shiro-tabs' );
+	const tab = blockElement.querySelector(
+		'[role="tab"][aria-selected="true"]'
+	);
+	const tabs = blockElement.querySelectorAll( '[role="tab"]' );
+
+	const currentTabIndex = Array.from( tabs ).findIndex(
+		( t ) => tab.getAttribute( 'id' ) === t.getAttribute( 'id' )
+	);
+
+	// Work out the next tab.
+	let nextTabIndex =
+		e.keyCode === 37 ? currentTabIndex - 1 : currentTabIndex + 1;
+	nextTabIndex = Math.min( Math.max( nextTabIndex, 0 ), tabs.length );
+
+	if ( nextTabIndex === currentTabIndex ) {
+		return;
+	}
+
+	activateTab( tabs[ nextTabIndex ] );
+	tabs[ currentTabIndex ].blur();
+	tabs[ nextTabIndex ].focus();
+}
+
+/**
+ * Initialize tab block.
+ *
+ * Ensure initial state is correct.
+ *
+ * @param {HTMLElement} blockElement Tab block.
+ */
+const initTabBlock = ( blockElement ) => {
+	const tabs = blockElement.querySelectorAll( '[role="tab"]' );
+	const tabList = blockElement.querySelector( '[role="tablist"]' );
+
+	// Add a click event handler to each tab
+	tabs.forEach( ( tab ) => {
+		// Use a scoped QS instead of getElementById to guard against malformed
+		// situations where same block instance renders twice on page.
+		const panel = blockElement.querySelector(
+			`#${ tab.getAttribute( 'aria-controls' ) }`
+		);
+		const isActive = tab.classList.contains(
+			'shiro-tabs__nav-button--active'
+		);
+
+		if ( isActive ) {
+			panel.removeAttribute( 'hidden' );
+		} else {
+			panel.setAttribute( 'hidden', true );
+		}
+
+		tab.addEventListener( 'click', handleTabClick );
+	} );
+
+	tabList.addEventListener( 'keydown', handleKeydown );
+};
+
+/**
+ * Kick it all off.
+ */
+const bootstrap = () => {
+	document
+		.querySelectorAll( '.shiro-tabs' )
+		.forEach( ( el ) => initTabBlock( el ) );
+};
+
+bootstrap();
